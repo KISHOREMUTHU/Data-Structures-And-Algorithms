70cbe04e84ea673fd8e69a45ab76e10e51120e62
Kishore Muthuselvan <kishoremuthuselvan@gmail.com>
2020-11-28 20:35:01 +0530

    Update matrix_binary_seach.c

--- matrix_binary_search.c
+++ matrix_binary_search.c
@@ -9,28 +9,52 @@ Here I will post my regular DSA problems
 
         
    int main (){
-      int a[3][3];
-     int i,j,x,l,r,mid,mid_el;
-     for(int i=0;i<3;i++){
-     for(int j=0;j<3;j++){
+
+      int a[3][3]; // 3x3 matrix for an example . If you wan , you can take any input matrix
+     
+      int i,j,x,l,r,mid,mid_el; // mid - middle value , mid_el - exact matrix equivalent value of mid
+
+    for(int i=0;i<3;i++){
+
+        for(int j=0;j<3;j++){
+
          scanf ("%d",&a[i][j]);
-         }
-        }
+
+                            }
+                         }
+
      scanf ("%d",&x);
+
      l=0;
      r=8;
-    while (l<=r){
+     
+     // Binary Search method
+
+     while (l<=r){
+
       mid=(l+r)/2;
-     mid_el=a[mid/3][mid%3];
+      mid_el=a[mid/3][mid%3]; // Formula to find the equivalent mid element in the matrix 
+
       if(mid_el==x){
+
        printf("%d",mid_el);
        break;
-      }
-    else if(mid_el<x){
+
+                   }
+
+      else if(mid_el<x){
+
          r=mid-1;
-        }
-        else{
-          l=mid+1;}
-     
+
+                       }
+
+      else{
+
+          l=mid+1;
+
           }
+                 }
+
+         return 0 ;
+
         }
